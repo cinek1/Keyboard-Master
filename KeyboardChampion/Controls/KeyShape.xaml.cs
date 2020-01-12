f6e8accf37f3b91fd4aed60d86c3ce4b654daa75
unknown <mgalas1@op.pl>
2020-01-12 13:59:41 +0100

    first commit

---
+++ KeyboardChampion/Controls/KeyShape.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KeyboardChampion.Controls
+{
+    /// <summary>
+    /// Interaction logic for KeyShape.xaml
+    /// </summary>
+    public partial class KeyShape : UserControl
+    {
+        public KeyShape()
+        {
+            InitializeComponent();
+            this.DataContext = this; 
+        }
+        public string Letter { get; set; }
+
+        public override string ToString()
+        {
+            return Letter;
+        }
+    }
+
+  
+     
+    
+}
---
+++ KeyboardChampion/Controls/Keyboard.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace KeyboardChampion.Controls
+{
+    /// <summary>
+    /// Interaction logic for Keyboard.xaml
+    /// </summary>
+    public partial class Keyboard : UserControl
+    {
+        private KeyShape lastType; 
+        public Dictionary<string, object> KeyToBorder;
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            window.KeyDown += Grid_KeyDown;
+        }
+
+
+        public Keyboard()
+        {
+            InitializeComponent();
+            KeyToBorder = new Dictionary<string, object>();
+            foreach (var child in Keyboards.Children)
+            {
+                KeyToBorder.Add(child.ToString(), child); 
+            }
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (lastType != null) lastType.KeyboardKey.Background = Brushes.White; 
+            KeyToBorder.TryGetValue(e.Key.ToString(), out object keyToMark);
+            lastType = keyToMark as KeyShape; 
+            if (lastType != null) lastType.KeyboardKey.Background = Brushes.Red;
+           
+        }
+    }
+}
